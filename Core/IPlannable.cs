7c7e0b5d9a2a9e673d406ea59a934ad86f5384e0
Hiroaki Kushida <kussy@live.jp>
2018-02-26 11:24:45 +0900

    #2 add 計画インターフェースを追加

---
+++ Core/IPlannable.cs
@@ -0,0 +1,22 @@
+﻿namespace Kussy.Analysis.Project.Core
+{
+    /// <summary>計画能力を与える</summary>
+    interface IPlannable
+    {
+        /// <summary>最早着手日を求める</summary>
+        /// <returns>最早着手日</returns>
+        LeadTime EarliestStart();
+        /// <summary>最早完了日を求める</summary>
+        /// <returns>最早完了日</returns>
+        LeadTime EarliestFinish();
+        /// <summary>最遅着手日を求める</summary>
+        /// <returns>最遅着手日</returns>
+        LeadTime LatestStart();
+        /// <summary>最遅完了日を求める</summary>
+        /// <returns>最遅完了日</returns>
+        LeadTime LatestFinish();
+        /// <summary>フロートを求める</summary>
+        /// <returns>フロート</returns>
+        LeadTime Float();
+    }
+}
