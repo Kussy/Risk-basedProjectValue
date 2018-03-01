b71c0706e6ab089422d47ceb75a6b3537e8e46d0
Hiroaki Kushida <kussy@live.jp>
2018-03-01 21:50:56 +0900

    #4 add 金銭クラスを実装

---
+++ Core/Currency.cs
@@ -0,0 +1,13 @@
+﻿namespace Kussy.Analysis.Project.Core
+{
+    /// <summary>通貨列挙型</summary>
+    public enum Currency
+    {
+        /// <summary>日本円</summary>
+        JPY = 0,
+        /// <summary>米ドル</summary>
+        USD = 1,
+        /// <summary>ユーロ</summary>
+        EUR = 2,
+    }
+}
--- Core/Money.cs
+++ Core/Money.cs
@@ -1,7 +1,31 @@
-﻿namespace Kussy.Analysis.Project.Core
+﻿using System.Collections.Generic;
+
+namespace Kussy.Analysis.Project.Core
 {
     /// <summary>抽象通貨クラス</summary>
-    public class Money
+    public class Money : ValueObject
     {
+        /// <summary>値</summary>
+        public decimal Value { get; private set; }
+        /// <summary>通貨</summary>
+        public Currency Currency { get; private set; }
+
+        /// <summary>プライベートコンストラクタ</summary>
+        private Money() { }
+        /// <summary>コンストラクタ</summary>
+        /// <param name="value">値</param>
+        /// <param name="currency">通貨</param>
+        public Money(decimal value, Currency currency)
+        {
+            Value = value;
+            Currency = currency;
+        }
+        /// <summary>プロパティを反復処理する</summary>
+        /// <returns></returns>
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Value;
+            yield return Currency;
+        }
     }
 }
