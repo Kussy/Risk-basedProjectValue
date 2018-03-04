769d207af96063e136b25ae00aa813d29cc9d342
Hiroaki Kushida <kussy@live.jp>
2018-03-04 22:20:16 +0900

    #6 update コンストラクタを静的ファクトリーメソッドに変更。プロパティ初期値を設定。

--- Core/Risk.cs
+++ Core/Risk.cs
@@ -3,26 +3,38 @@
 namespace Kussy.Analysis.Project.Core
 {
     /// <summary>リスク</summary>
-    public class Risk:ValueObject
+    public class Risk : ValueObject
     {
         /// <summary>失敗確率</summary>
-        public decimal FailRate { get; private set; }
+        public decimal FailRate { get; private set; } = 0m;
         /// <summary>リワーク確率</summary>
-        public decimal ReworkRate { get; private set; }
+        public decimal ReworkRate { get; private set; } = 0m;
         /// <summary>コスト超過確率</summary>
-        public decimal CostOverRate { get; private set; }
+        public decimal CostOverRate { get; private set; } = 0m;
 
         /// <summary>プライベートコンストラクタ</summary>
         private Risk() { }
-        /// <summary>コンストラクタ</summary>
+
+        /// <summary>静的ファクトリーメソッド</summary>
+        /// <returns>インスタンス初期値</returns>
+        public static Risk Of()
+        {
+            return new Risk();
+        }
+
+        /// <summary>静的ファクトリーメソッド</summary>
         /// <param name="failRate">失敗確率</param>
         /// <param name="reworkRate">リワーク確率</param>
         /// <param name="costOverRate">コスト超過確率</param>
-        public Risk(decimal failRate, decimal reworkRate, decimal costOverRate)
+        /// <returns>パラメータと同じ値を持つインスタンス</returns>
+        public static Risk Of(decimal failRate, decimal reworkRate, decimal costOverRate)
         {
-            FailRate = failRate;
-            ReworkRate = reworkRate;
-            CostOverRate = costOverRate;
+            return new Risk()
+            {
+                FailRate = failRate,
+                ReworkRate = reworkRate,
+                CostOverRate = costOverRate,
+            };
         }
 
         /// <summary>プロパティを反復処理する</summary>
