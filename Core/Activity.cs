bc0e4cd777b993d0f2d790516800c6314b81e39b
Hiroaki Kushida <kussy@live.jp>
2018-03-04 14:46:45 +0900

    #6 add アクティビティクラスの追加。進捗インターフェースを実装。

---
+++ Core/Activity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Kussy.Analysis.Project.Core
+{
+    /// <summary>作業を表現するクラス</summary>
+    /// <remarks>非常に多くの能力を持つために肥大化した場合を検討しておく</remarks>
+    public class Activity : IProgressable
+    {
+        /// <summary>進捗状態</summary>
+        public State State { get; private set; } = State.ToDo;
+
+        /// <summary>進捗を確認する</summary>
+        /// <param name="state">進捗状態</param>
+        public void Progress(State state)
+        {
+            State = state;
+        }
+    }
+}
---
+++ CoreTest/TestActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kussy.Analysis.Project.Core
+{
+    [TestClass]
+    public class TestActivity
+    {
+        [TestMethod]
+        public void アクティビティの初期状態は未着手であるべき()
+        {
+            var activity = new Activity();
+            activity.State.Is(State.ToDo);
+        }
+
+        [TestMethod]
+        public void アクティビティの進捗は報告された状態であるべき()
+        {
+            var activity = new Activity();
+            activity.Progress(State.Doing);
+            activity.State.Is(State.Doing);
+            activity.Progress(State.Done);
+            activity.State.Is(State.Done);
+        }
+    }
+}
