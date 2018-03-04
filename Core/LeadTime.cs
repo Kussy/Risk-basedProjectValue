cb05d3d352fc2c762bee7dcca0ffbbd97f3b6506
Hiroaki Kushida <kussy@live.jp>
2018-03-04 22:35:07 +0900

    #6 update コンストラクタを静的ファクトリーメソッドに変更。プロパティ初期値を設定。前提条件を追加。

--- Core/LeadTime.cs
+++ Core/LeadTime.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 
 namespace Kussy.Analysis.Project.Core
 {
@@ -7,20 +7,34 @@ namespace Kussy.Analysis.Project.Core
     public class LeadTime : ValueObject
     {
         /// <summary>値</summary>
-        public decimal Value { get; private set; }
+        public decimal Value { get; private set; } = 0m;
         /// <summary>単位</summary>
-        public TimeType Unit { get; private set; }
+        public TimeType Unit { get; private set; } = TimeType.Day;
 
         /// <summary>プライベートコンストラクタ</summary>
         private LeadTime() { }
-        /// <summary>コンストラクタ</summary>
+
+        /// <summary>静的ファクトリーメソッド</summary>
+        /// <returns>インスタンス初期値</returns>
+        public static LeadTime Of()
+        {
+            return new LeadTime();
+        }
+
+        /// <summary>静的ファクトリーメソッド</summary>
         /// <param name="value">値</param>
         /// <param name="unit">単位</param>
-        public LeadTime(decimal value, TimeType unit)
+        /// <returns>パラメータと同じ値を持つインスタンス</returns>
+        public static LeadTime Estemated(decimal value, TimeType unit)
         {
-            Value = value;
-            Unit = unit;                
+            Contract.Requires(value >= 0);
+            return new LeadTime()
+            {
+                Value = value,
+                Unit = unit,
+            };
         }
+
         /// <summary>プロパティを反復処理する</summary>
         /// <returns>要素列挙</returns>
         protected override IEnumerable<object> GetAtomicValues()
