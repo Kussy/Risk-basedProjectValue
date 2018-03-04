041b67d7d3c2593e4cf2fab3141364ac30a454ef
Hiroaki Kushida <kussy@live.jp>
2018-03-04 22:23:35 +0900

    #6 update コンストラクタを静的ファクトリーメソッドに変更。プロパティ初期値を設定。

--- Core/WorkLoad.cs
+++ Core/WorkLoad.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 
 namespace Kussy.Analysis.Project.Core
 {
@@ -6,23 +7,36 @@ namespace Kussy.Analysis.Project.Core
     public class WorkLoad : ValueObject
     {
         /// <summary>値</summary>
-        public decimal Value { get; private set; }
+        public decimal Value { get; private set; } = 0m;
         /// <summary>時間種別</summary>
-        public TimeType TimeUnit { get; private set; }
+        public TimeType TimeUnit { get; private set; } = TimeType.Day;
         /// <summary>作業者種別</summary>
-        public WorkerType WorkerUnit { get; private set; }
+        public WorkerType WorkerUnit { get; private set; } = WorkerType.Human;
 
         /// <summary>プライベートコンストラクタ</summary>
         private WorkLoad() { }
-        /// <summary>コンストラクタ</summary>
+
+        /// <summary>静的ファクトリーメソッド</summary>
+        /// <returns>インスタンス初期値</returns>
+        public static WorkLoad Of()
+        {
+            return new WorkLoad();
+        }
+
+        /// <summary>静的ファクトリーメソッド</summary>
         /// <param name="value">値</param>
         /// <param name="timeUnit">時間種別</param>
         /// <param name="workerUnit">作業者種別</param>
-        public WorkLoad(decimal value, TimeType timeUnit, WorkerType workerUnit)
+        /// <returns>パラメータと同じ値を持つインスタンス</returns>
+        public static WorkLoad Of(decimal value, TimeType timeUnit, WorkerType workerUnit)
         {
-            Value = value;
-            TimeUnit = timeUnit;
-            WorkerUnit = workerUnit;
+            Contract.Requires(value >= 0);
+            return new WorkLoad
+            {
+                Value = value,
+                TimeUnit = timeUnit,
+                WorkerUnit = workerUnit
+            };
         }
         /// <summary>プロパティを反復処理する</summary>
         /// <returns>要素列挙</returns>
