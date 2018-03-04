1d28a274b80a1f43261752f667bd0943dab9cd57
Hiroaki Kushida <kussy@live.jp>
2018-03-04 22:50:03 +0900

    #6 update 見積インターフェースを実装。テストも追加。

--- CoreTest/TestActivity.cs
+++ CoreTest/TestActivity.cs
@@ -22,5 +22,105 @@ namespace Kussy.Analysis.Project.Core
             activity.Progress(State.Done);
             activity.State.Is(State.Done);
         }
+
+        [TestMethod]
+        public void 見積前のアクティビティの収入はゼロであるべき()
+        {
+            var activity = new Activity();
+            activity.Income.Value.Is(0m);
+        }
+
+        [TestMethod]
+        public void 見積後のアクティビティの収入は与えられたものであるべき()
+        {
+            var expectedValue = 100m;
+            var expectedCurrency = Currency.USD;
+            var activity = new Activity();
+            activity.Estimate(Income.Of(expectedValue, expectedCurrency));
+            activity.Income.Value.Is(expectedValue);
+            activity.Income.Currency.Is(expectedCurrency);
+        }
+
+        [TestMethod]
+        public void 見積前のアクティビティの支出はゼロであるべき()
+        {
+            var activity = new Activity();
+            activity.DirectCost.Value.Is(0m);
+        }
+
+        [TestMethod]
+        public void 見積後のアクティビティの支出は与えられたものであるべき()
+        {
+            var expectedValue = 100m;
+            var expectedCurrency = Currency.USD;
+            var activity = new Activity();
+            activity.Estimate(Cost.Of(expectedValue, expectedCurrency));
+            activity.DirectCost.Value.Is(expectedValue);
+            activity.DirectCost.Currency.Is(expectedCurrency);
+        }
+
+        [TestMethod]
+        public void 見積前のアクティビティの作業量はゼロであるべき()
+        {
+            var activity = new Activity();
+            activity.WorkLoad.Value.Is(0m);
+        }
+
+        [TestMethod]
+        public void 見積後のアクティビティの作業量は与えられたものであるべき()
+        {
+            var expectedValue = 100m;
+            var expectedTimeType = TimeType.Day;
+            var expectedWorkerType = WorkerType.Human;
+
+            var activity = new Activity();
+            activity.Estimate(WorkLoad.Of(expectedValue, expectedTimeType, expectedWorkerType));
+            activity.WorkLoad.Value.Is(expectedValue);
+            activity.WorkLoad.TimeUnit.Is(expectedTimeType);
+            activity.WorkLoad.WorkerUnit.Is(expectedWorkerType);
+        }
+
+        [TestMethod]
+        public void 見積前のアクティビティの固定時間はゼロであるべき()
+        {
+            var activity = new Activity();
+            activity.FixTime.Value.Is(0m);
+            activity.FixTime.TimeUnit.Is(TimeType.Day);
+        }
+
+        [TestMethod]
+        public void 見積後のアクティビティの固定時間は与えられたものであるべき()
+        {
+            var expectedValue = 100m;
+            var expectedTimeType = TimeType.Day;
+
+            var activity = new Activity();
+            activity.Estimate(LeadTime.Of(expectedValue, expectedTimeType));
+            activity.FixTime.Value.Is(expectedValue);
+            activity.FixTime.TimeUnit.Is(expectedTimeType);
+        }
+
+        [TestMethod]
+        public void 見積前のアクティビティのリスクはゼロであるべき()
+        {
+            var activity = new Activity();
+            activity.Risk.FailRate.Is(0m);
+            activity.Risk.CostOverRate.Is(0m);
+            activity.Risk.ReworkRate.Is(0m);
+        }
+
+        [TestMethod]
+        public void 見積後のアクティビティのリスクは与えられたものであるべき()
+        {
+            var expectedFailRate= 0.3m;
+            var expectedReworkRate = 0.4m;
+            var expectedCostOverRate = 0.5m;
+
+            var activity = new Activity();
+            activity.Estimate(Risk.Of(expectedFailRate, expectedReworkRate, expectedCostOverRate));
+            activity.Risk.FailRate.Is(expectedFailRate);
+            activity.Risk.ReworkRate.Is(expectedReworkRate);
+            activity.Risk.CostOverRate.Is(expectedCostOverRate);
+        }
     }
 }
