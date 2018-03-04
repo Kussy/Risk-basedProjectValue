8216476751ce017d787148ae7a6484cc657cd3fd
Hiroaki Kushida <kussy@live.jp>
2018-03-04 22:38:16 +0900

    #6 update コンストラクタを静的ファクトリーメソッドに変更。前提条件を追加。

--- Core/Cost.cs
+++ Core/Cost.cs
@@ -1,11 +1,41 @@
-﻿namespace Kussy.Analysis.Project.Core
+﻿using System.Diagnostics.Contracts;
+
+namespace Kussy.Analysis.Project.Core
 {
     /// <summary>支出</summary>
     public class Cost : Money
     {
-        /// <summary>コンストラクタ</summary>
+        /// <summary>プライベートコンストラクタ</summary>
+        protected Cost() : base() { }
+
+        /// <summary>基底クラスからのキャスト用のコンストラクタ</summary>
+        /// <param name="money">金銭</param>
+        /// <returns>パラメータと同じ値を持つインスタンス</returns>
+        protected static Cost Of(Money money)
+        {
+            return new Cost()
+            {
+                Value = money.Value,
+                Currency = money.Currency,
+            };
+        }
+
+        /// <summary>静的ファクトリーメソッド</summary>
+        /// <returns>インスタンス初期値</returns>
+        public new static Cost Of()
+        {
+            return Of(Money.Of());
+        }
+
+        /// <summary>静的ファクトリーメソッド</summary>
         /// <param name="value">値</param>
         /// <param name="currency">通貨</param>
-        public Cost(decimal value, Currency currency) : base(value, currency) { }
+        /// <returns>パラメータと同じ値を持つインスタンス</returns>
+        public new static Cost Of(decimal value, Currency currency)
+        {
+            Contract.Requires(value >= 0);
+
+            return Of(Money.Of(value, currency));
+        }
     }
 }
