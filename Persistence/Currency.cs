a84a20788f524e6c82b122456127dc0ff247bbb6
Hiroaki Kushida <kussy@live.jp>
2018-03-23 12:00:57 +0900

    #22 add enum型を定義

---
+++ Persistence/Currency.cs
@@ -0,0 +1,16 @@
+﻿namespace Kussy.Analysis.Project.Persistence
+{
+    /// <summary>通貨列挙型</summary>
+    /// <remarks>ISO 4217を参照のこと</remarks>
+    public enum CurrencyType
+    {
+        /// <summary>通貨なし</summary>
+        XXX = 0,
+        /// <summary>日本円</summary>
+        JPY = 1,
+        /// <summary>米ドル</summary>
+        USD = 2,
+        /// <summary>ユーロ</summary>
+        EUR = 3,
+    }
+}
