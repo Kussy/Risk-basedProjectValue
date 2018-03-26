5f7c077d1867e16a295c4c6364d99a89fb7c493d
Hiroaki Kushida <kussy@live.jp>
2018-03-26 12:35:46 +0900

    #26 add ネットワーク操作を追加

---
+++ Persistence/Controller/NetworkController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Kussy.Analysis.Project.Persistence
+{
+    /// <summary>ネットワーク操作</summary>
+    public class NetworkController
+    {
+        /// <summary>DBコンテキスト</summary>
+        public RpvDbContext Context { get; private set; }
+
+        /// <summary>コンストラクタ隠蔽</summary>
+        private NetworkController() { }
+
+        /// <summary>DBコンテキストの依存性注入</summary>
+        /// <param name="context">DBコンテキスト</param>
+        public NetworkController(RpvDbContext context)
+        {
+            Context = context;
+        }
+
+        /// <summary>ネットワークの作成</summary>
+        /// <param name="ancestor">先祖</param>
+        /// <param name="descendant">子孫</param>
+        /// <param name="depth">深さ</param>
+        public void Create(Activity ancestor, Activity descendant, int depth)
+        {
+            var network = new Network
+            {
+                Ancestor = ancestor,
+                Descendant = descendant,
+                Depth = depth,
+            };
+            Context.Networks.Add(network);
+            Context.SaveChanges();
+        }
+
+        /// <summary>ネットワークの取得</summary>
+        /// <param name="ancestor">先祖</param>
+        /// <param name="descendant">子孫</param>
+        /// <returns>ネットワーク</returns>
+        public Network Read(Activity ancestor, Activity descendant)
+        {
+            var network = Context.Networks.Find(ancestor.Id, descendant.Id);
+            if (network is null) return network;
+            Context.Entry(network).Reference(a => a.Ancestor).Load();
+            Context.Entry(network.Ancestor).Reference(a => a.Project).Load();
+            Context.Entry(network.Ancestor).Collection(a => a.Assigns).Load();
+            foreach (var assign in network.Ancestor.Assigns)
+            {
+                Context.Entry(assign).Reference(a => a.Resource).Load();
+            }
+            Context.Entry(network).Reference(a => a.Descendant).Load();
+            Context.Entry(network.Descendant).Reference(a => a.Project).Load();
+            Context.Entry(network.Descendant).Collection(a => a.Assigns).Load();
+            foreach (var assign in network.Descendant.Assigns)
+            {
+                Context.Entry(assign).Reference(a => a.Resource).Load();
+            }
+
+            return network;
+        }
+
+        /// <summary>ネットワークの更新</summary>
+        /// <param name="network">ネットワーク</param>
+        public void Update(Network network)
+        {
+            var findNetwork = Context.Networks.Find(network.AncestorId, network.DescendantId);
+            findNetwork.Depth = network.Depth;
+            Context.SaveChanges();
+        }
+
+        /// <summary>ネットワークの削除の削除</summary>
+        /// <param name="network">ネットワーク</param>
+        public void Delete(Network network)
+        {
+            var findNetwork = Context.Networks.Find(network.AncestorId, network.DescendantId);
+            Context.Networks.Remove(findNetwork);
+            Context.SaveChanges();
+        }
+    }
+}
---
+++ PersistenceTest/TestNetworkController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Kussy.Analysis.Project.Persistence
+{
+    [TestClass]
+    public class TestNetworkController
+    {
+        SqliteConnection Connection { get; set; }
+        DbContextOptions<RpvDbContext> Options { get; set; }
+        RpvDbContext DbContext { get; set; }
+        ProjectController ProjectController { get; set; }
+        ActivityController ActivityController { get; set; }
+        NetworkController NetworkController { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Connection = new SqliteConnection("DataSource=:memory:");
+            Connection.Open();
+            Options = new DbContextOptionsBuilder<RpvDbContext>().UseSqlite(Connection).Options;
+            DbContext = new RpvDbContext(Options);
+            DbContext.Database.EnsureCreated();
+            ProjectController = new ProjectController(DbContext);
+            ActivityController = new ActivityController(DbContext);
+            NetworkController = new NetworkController(DbContext);
+        }
+
+        [TestCleanup]
+        public void Creanup()
+        {
+            DbContext.Database.EnsureDeleted();
+            DbContext.Dispose();
+        }
+
+        [TestMethod]
+        public void ネットワークのCRUD操作を一通り()
+        {
+            var code = "ancestor";
+            var name = "ancestor";
+            var expectedDepth = 0;
+            ProjectController.Create(code, name);
+            var project = ProjectController.Read(code);
+            ActivityController.Create(project, code, name);
+            var activity = ActivityController.Read(code);
+ 
+            NetworkController.Create(activity, activity, expectedDepth);
+            var selfNetwork = NetworkController.Read(activity, activity);
+            selfNetwork.AncestorId.Is(activity.Id);
+            selfNetwork.DescendantId.Is(activity.Id);
+            selfNetwork.Depth.Is(expectedDepth);
+
+            var changedDepth = 1;
+            selfNetwork.Depth = changedDepth;
+            NetworkController.Update(selfNetwork);
+
+            var changedNetwork = NetworkController.Read(activity, activity);
+            changedNetwork.Depth.Is(changedDepth);
+
+            NetworkController.Delete(changedNetwork);
+            NetworkController.Read(activity, activity).IsNull();
+        }
+    }
+}
