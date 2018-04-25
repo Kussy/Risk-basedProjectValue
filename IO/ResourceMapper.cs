e718ad21a2a666c9c2b06aa669c70573e7f2722c
Hiroaki Kushida <kussy@live.jp>
2018-04-25 17:49:29 +0900

    #32 add 資源ファイルの読込を追加

---
+++ IO/ResourceMapper.cs
@@ -0,0 +1,59 @@
+﻿using CsvHelper.Configuration;
+using Kussy.Analysis.Project.Persistence;
+
+namespace Kussy.Analysis.Project.IO
+{
+    /// <summary>資源クラスとのマッピング定義</summary>
+    public class ResourceMapper : ClassMap<Resource>
+    {
+        /// <summary></summary>
+        private int IndexForId => 0;
+        /// <summary></summary>
+        private int IndexForCode => 1;
+        /// <summary></summary>
+        private int IndexForName => 2;
+        /// <summary></summary>
+        private int IndexForType => 3;
+        /// <summary></summary>
+        private int IndexForProductivity => 4;
+
+        /// <summary></summary>
+        private string NameForId => "Id";
+        /// <summary></summary>
+        private string NameForCode => "Code";
+        /// <summary></summary>
+        private string NameForName => "Name";
+        /// <summary></summary>
+        private string NameForType => "Type";
+        /// <summary></summary>
+        private string NameForProductivity => "Productivity";
+
+
+        /// <summary>コンストラクタ</summary>
+        public ResourceMapper()
+        {
+            Map(x => x.Id)
+                .Name(NameForId)
+                .Index(IndexForId)
+                .Validate(f => int.TryParse(f, out var i));
+            Map(x => x.Code)
+                .Name(NameForCode)
+                .Index(IndexForCode)
+                .Validate(f => !string.IsNullOrEmpty(f));
+            Map(x => x.Name)
+                .Name(NameForName)
+                .Index(IndexForName)
+                .Validate(f => !string.IsNullOrEmpty(f));
+            Map(x => x.Type)
+                .Name(NameForType)
+                .Index(IndexForType)
+                .Validate(f => int.TryParse(f, out var i) && i >= 0 && i <= 2)
+                .Default(ResourceType.Unknown);
+            Map(x => x.Productivity)
+                .Name(NameForProductivity)
+                .Index(IndexForProductivity)
+                .Validate(f => decimal.TryParse(f, out var d) && d > 0)
+                .Default(1.0m);
+        }
+    }
+}
---
+++ IO/RpvCsvReader.cs
@@ -0,0 +1,31 @@
+﻿using CsvHelper;
+using Kussy.Analysis.Project.Persistence;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Kussy.Analysis.Project.IO
+{
+    /// <summary></summary>
+    public sealed class RpvCsvReader
+    {
+        /// <summary>シングルトンインスタンス</summary>
+        private static RpvCsvReader _instance = new RpvCsvReader();
+        /// <summary>シングルトンプロパティ</summary>
+        public static RpvCsvReader Instance => _instance;
+        /// <summary>プライベートコンストラクタ</summary>
+        private RpvCsvReader() { }
+
+        /// <summary>資源ファイルの読込</summary>
+        /// <param name="csvFilePath">CSVファイルパス</param>
+        /// <returns>資源群</returns>
+        public IEnumerable<Resource> GetResources(string csvFilePath)
+        {
+            using (var streamReader = new StreamReader(csvFilePath))
+            using (var csv = new CsvReader(streamReader))
+            {
+                csv.Configuration.RegisterClassMap<ResourceMapper>();
+                return csv.GetRecords<Resource>();
+            }
+        }
+    }
+}
