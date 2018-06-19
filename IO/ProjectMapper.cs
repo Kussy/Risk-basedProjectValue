5225453120d701f65a4b0fb3bca180f05ca45a3f
Hiroaki Kushida <kussy@live.jp>
2018-06-19 17:10:05 +0900

    #32 add プロジェクトファイルの読込を追加

---
+++ IO/ProjectMapper.cs
@@ -0,0 +1,87 @@
+﻿using CsvHelper.Configuration;
+using Kussy.Analysis.Project.Persistence;
+
+namespace Kussy.Analysis.Project.IO
+{
+    /// <summary>資源クラスとのマッピング定義</summary>
+    public class ProjectMapper : ClassMap<Persistence.Project>
+    {
+        /// <summary></summary>
+        private int IndexForId => 0;
+        /// <summary></summary>
+        private int IndexForCode => 1;
+        /// <summary></summary>
+        private int IndexForName => 2;
+        /// <summary></summary>
+        private int IndexForUnitOfCurrency => 3;
+        /// <summary></summary>
+        private int IndexForUnitOfTime => 4;
+        /// <summary></summary>
+        private int IndexForTerm => 5;
+        /// <summary></summary>
+        private int IndexForBudjet => 6;
+        /// <summary></summary>
+        private int IndexForLiquidatedDamages => 7;
+
+        /// <summary></summary>
+        private string NameForId => "Id";
+        /// <summary></summary>
+        private string NameForCode => "Code";
+        /// <summary></summary>
+        private string NameForName => "Name";
+        /// <summary></summary>
+        private string NameForUnitOfCurrency => "UnitOfCurrency";
+        /// <summary></summary>
+        private string NameForUnitOfTime => "UnitOfTime";
+        /// <summary></summary>
+        private string NameForTerm => "Term";
+        /// <summary></summary>
+        private string NameForBudjet => "Budjet";
+        /// <summary></summary>
+        private string NameForLiquidatedDamages => "LiquidatedDamages";
+
+
+        /// <summary>コンストラクタ</summary>
+        public ProjectMapper()
+        {
+            Map(x => x.Id)
+                .Name(NameForId)
+                .Index(IndexForId)
+                .Validate(f => string.IsNullOrEmpty(f) || int.TryParse(f, out var i))
+                .Default(-1);
+            Map(x => x.Code)
+                .Name(NameForCode)
+                .Index(IndexForCode)
+                .Validate(f => !string.IsNullOrEmpty(f));
+            Map(x => x.Name)
+                .Name(NameForName)
+                .Index(IndexForName)
+                .Validate(f => !string.IsNullOrEmpty(f));
+            Map(x => x.UnitOfCurrency)
+                .Name(NameForUnitOfCurrency)
+                .Index(IndexForUnitOfCurrency)
+                .Validate(f => string.IsNullOrEmpty(f) || (int.TryParse(f, out var i) && i >= 0 && i <= 3))
+                .Default(CurrencyType.JPY);
+            Map(x => x.UnitOfTime)
+                .Name(NameForUnitOfTime)
+                .Index(IndexForUnitOfTime)
+                .Validate(f => string.IsNullOrEmpty(f) || (int.TryParse(f, out var i) && i >= 0 && i <= 7))
+                .Default(TimeType.Day);
+            Map(x => x.Term)
+                .Name(NameForTerm)
+                .Index(IndexForTerm)
+                .Validate(f => string.IsNullOrEmpty(f) || (decimal.TryParse(f, out var d) && d >= 0))
+                .Default(0m);
+            Map(x => x.Budjet)
+                .Name(NameForBudjet)
+                .Index(IndexForBudjet)
+                .Validate(f => string.IsNullOrEmpty(f) || (decimal.TryParse(f, out var d) && d >= 0))
+                .Default(0m);
+            Map(x => x.LiquidatedDamages)
+                .Name(NameForLiquidatedDamages)
+                .Index(IndexForLiquidatedDamages)
+                .Validate(f => string.IsNullOrEmpty(f) || (decimal.TryParse(f, out var d) && d >= 0))
+                .Default(0m);
+        }
+    }
+}
--- IO/RpvCsvReader.cs
+++ IO/RpvCsvReader.cs
@@ -15,6 +15,26 @@ namespace Kussy.Analysis.Project.IO
         /// <summary>プライベートコンストラクタ</summary>
         private RpvCsvReader() { }
 
+        /// <summary>プロジェクトファイルの読込</summary>
+        /// <param name="csvFilePath">CSVファイルパス</param>
+        /// <returns>プロジェクト群</returns>
+        public IEnumerable<Persistence.Project> GetProjects(string csvFilePath)
+        {
+            using (var streamReader = new StreamReader(csvFilePath))
+            using (var csv = new CsvReader(streamReader))
+            {
+                var records = new List<Persistence.Project>();
+                csv.Configuration.RegisterClassMap<ProjectMapper>();
+                csv.Read();
+                csv.ReadHeader();
+                while (csv.Read())
+                {
+                    records.Add(csv.GetRecord<Persistence.Project>());
+                }
+                return records;
+            }
+        }
+
         /// <summary>資源ファイルの読込</summary>
         /// <param name="csvFilePath">CSVファイルパス</param>
         /// <returns>資源群</returns>
