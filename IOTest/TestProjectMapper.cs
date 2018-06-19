dc35b928893b4432f4dd444ec302b869da0e740f
Hiroaki Kushida <kussy@live.jp>
2018-06-19 17:10:47 +0900

    #32 add プロジェクトCSVの読取を作成

---
+++ IOTest/TestProjectMapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kussy.Analysis.Project.IO
+{
+    [TestClass]
+    public class TestProjectMapper
+    {
+        [TestMethod]
+        [DeploymentItem(@"TestData\")]
+        public void CSVとしての標準仕様とマッピング設定が機能しているべき()
+        {
+            var projects = RpvCsvReader.Instance.GetProjects("Projects.csv");
+            projects.Count().Is(6);
+        }
+    }
+}
